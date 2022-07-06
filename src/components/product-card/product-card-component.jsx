c02b005ab6cac532c372488e056a0c46de11de17
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-07-06 20:22:21 +0700

    added Logic for CartItem and added cartItem list

--- src/components/product-card/product-card-component.jsx
+++ src/components/product-card/product-card-component.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import "./product-card.styles.scss";
 const ProductsCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +14,9 @@ const ProductsCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to Card</Button>
+      <Button onClick={addProductToCart} buttonType="inverted">
+        Add to Card
+      </Button>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
 import { createContext } from "react";
 
+//! untuk logic jika produk sama
+const addcCartItem = (cartItems, productToAdd) => {
+  //* mencari cartItem terdapat id yga sama dengan productToadd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  //* jika ada , increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //* return array baru modifed dari carItems/new cart item
+  //! handle for a new product
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setisCartOpen] = useState(false);
-  const value = { isCartOpen, setisCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addcCartItem(cartItems, productToAdd));
+  };
+  const value = { isCartOpen, setisCartOpen, addItemToCart, cartItems };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
